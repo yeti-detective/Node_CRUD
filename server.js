d05405c26adc4b8743871d1638d308011b9203a9
yeti-detective <yeti.detective@gmail.com>
2017-01-08 12:54:36 -0500

    probably final commit?

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongo = require('mongodb').MongoClient;
+
+var port = 8080;
+var db;
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray((err, result) => {
+        if (err) throw err;
+        res.render('index.ejs', {quotes: result});
+    })
 });
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body);
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err);
+        
+        console.log('saved to database');
+        res.redirect('/');
+    })
 });
 
-app.listen(8080, function() {
-    console.log('listening on 8080');
-});
 
+mongo.connect('mongodb://quote-app:Quote-App@ds157278.mlab.com:57278/yetis_first_db', (err, database) => {
+    if(err){throw err};
+    db = database;
+
+    app.listen(port, function() {
+    console.log('listening on ' + port.toString());
+    });
+
+    db.collection('quotes').find().toArray((err, results) => {
+        console.log(results);
+    })
+
+})
