91cf686031ae9040c24945608c790d42632a1dfc
Christopher M Brown <yeti.detective@gmail.com>
2017-01-08 14:24:17 +0000

    here we go, yo

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
-})
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
-    console.log('listening on 3000');
-})
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(8080, function() {
+    console.log('listening on 8080');
+});
 
